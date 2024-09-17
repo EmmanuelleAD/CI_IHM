3a33efbf222c04aaeaaa15198633ef47ee1402f9
lamya1baidouri <alibaidouri7@gmail.com>
2024-09-17 23:59:10 +0200

    Add customer-count component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {CustomerCountComponent} from "./Customer_count/customer-count/customer-count.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/customer-count', pathMatch: 'full' },
+  { path: 'customer-count', component: CustomerCountComponent},
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],  // Configurez les routes avec RouterModule
+  exports: [RouterModule]  // Exportez RouterModule pour l'utiliser dans d'autres modules
+})
+export class AppRoutingModule { }
