87740ce8d8b59b0f486e23f6751ed06b7865145b
bilal-mesaouri <bilmsr.bm@gmail.com>
2024-09-17 11:20:54 +0200

    basic router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TableReservationComponent } from './features/reservation/table-reservation/table-reservation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'table-reservation', component: TableReservationComponent },
+];
+
