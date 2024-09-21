fe386fbb4516aed933543f215016d5ade1b7b23d
EmmanuelleAD <emmanuelleadote@gmail.com>
2024-09-21 12:06:28 +0200

    réaliser la page des articles commandés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemsOrderedComponent } from '../items-ordered/items-ordered.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ItemsOrderedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
