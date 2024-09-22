c354b96e98e855f8237d1c014bb3ecfd1e4b77ef
EmmanuelleAD <emmanuelleadote@gmail.com>
2024-09-22 17:37:02 +0200

    ajout du modèle category et de dépendances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,18 @@ import { MenuComponent } from './components/menu/menu.component';
 import { TableReservationComponent } from './components/table-reservation/table-reservation.component';
 import { HomeComponent } from './components/home/home.component';
 import {CustomerCountComponent} from "./Customer_count/customer-count/customer-count.component";
+import {CategoryComponent} from "./components/category/category.component";
+import {TableCategoriesComponent} from "./components/table-categories/table-categories.component";
+import {HeaderComponent} from "./components/header/header.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'menu', component: MenuComponent},
     { path: 'table-reservation', component: TableReservationComponent },
-  { path: 'customer-count', component: CustomerCountComponent }
-
-
+  { path: 'customer-count', component: CustomerCountComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'table-categories', component: TableCategoriesComponent },
+  { path: 'header', component: HeaderComponent }
 ];
 
 @NgModule({
---
+++ src/app/constants.ts
@@ -0,0 +1,19 @@
+import {Category} from "./interfaces/Category";
+
+export const DRINKS:string="Drinks"
+export const STARTERS:string="Starters"
+export const MAIN:string="Main Course"
+export const CATEGORIES:Category[]=[
+  {
+    title:"Drinks",
+    path:"https://plus.unsplash.com/premium_photo-1679436985567-24325ae4bbf7?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    title:"Starters",
+    path:"https://st5.depositphotos.com/1027198/67803/i/380/depositphotos_678038000-stock-photo-festive-christmas-new-year-canape.jpg"
+  },
+  {
+    title:"Main Course",
+    path:"https://cdn.pixabay.com/photo/2020/09/16/20/59/hamburger-5577498_1280.jpg"
+  },
+]
