41f9175015bae51de0f82c70e85f5a45b59b9952
EmmanuelleAD <emmanuelleadote@gmail.com>
2024-10-06 01:42:05 +0200

    css table-categories.component.scss

--- src/app/components/table-reservation/table-reservation.component.ts
+++ src/app/components/table-reservation/table-reservation.component.ts
@@ -152,7 +152,7 @@ this.serviceTable.createTableOrder(t.number,1).subscribe();
 
   navigateToNextPage() {
     this.repartirClientsSurTables(this.numberOfCustomers,this.tableNumberGlobal,4).subscribe(()=>{
-      this.router.navigate(['/menu']).then(()=>this.orderService.filterAndOrganizeOrders(this.tableNumberGlobal.toString()).subscribe(ordersMap=>{
+      this.router.navigate(['/table-categories']).then(()=>this.orderService.filterAndOrganizeOrders(this.tableNumberGlobal.toString()).subscribe(ordersMap=>{
         this.store.dispatch(setCommands({orderDictionary:ordersMap,commandNumber:this.tableNumberGlobal}))
         console.log("order",ordersMap)
       }));
